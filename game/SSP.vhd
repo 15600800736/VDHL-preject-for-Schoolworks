5abe90dae68619ff086ac2e9caeb89b7b074261c
15600800736 <183425990@qq.com>
2016-10-20 23:15:55 +0800

    uncomplete digitron

--- game/SSP.vhd
+++ game/SSP.vhd
@@ -52,15 +52,14 @@ end behave;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-entity SSP is
+entity control is
+	--game stype:a select,b select and state to wait ready
 	port(
 		clk_in:in std_logic;
 		--game start signal
-		reset:in std_logic;
+		start:in std_logic;
 		--game ready signal
 		ready:in std_logic;
-		--judge and show signal
-		show:in std_logic;
 		--A and B select signal
 		-- 0 for stone
 		-- 1 for scissors
@@ -72,87 +71,78 @@ entity SSP is
 		--0 no result
 		--1 A win
 		--2 B win
-		result:out integer range 0 to 2;
-		--signal to tell matrix show A and B's selection
-		a_select_out,b_select_out:out integer range 0 to 2;
-		--A and B score to tell the digitron to show
-		a_score_out,b_score_out:out integer range 0 to 3
+		a_score_out,b_score_out:out integer range 0 to 2;
+		result:out integer range 0 to 2		
 		);
-end SSP;
+end control;
 
 
-architecture behave of SSP is
-		--game stype:a select,b select and state to wait ready
-		type state_type is (s0,a,b,judge,over);
-		signal game_state:state_type;
+architecture behave of control is
+		type state_type is (s0,s1,a,b);
+		signal game_state:state_type:= s0;
 		--A and B score to tell the digitron to show
-		signal a_score,b_score:integer range 0 to 3;
 		--intialize
 		begin
-		process(clk_in,reset)
+		process(clk_in,start)
+		variable a_score,b_score:integer range 0 to 3;
 		begin
-			if(reset = '1')then
-				a_score <= 0;
-				b_score <= 0;
-				result <= 0;
-				game_state <= s0;
-			elsif(clk_in'event and clk_in = '1')then 
+			if(clk_in'event and clk_in = '1')then 
 				case game_state is
 				when s0=>
+					if(start = '1')then
+						game_state <= s1;
+					end if;
+				when s1=>
 					if(ready = '1')then
 						game_state <= a;
 					end if;
 				when a=>
 					if(a_ok = '1')then
-						a_select_out <= a_select_in;
 						game_state <= b;
 					end if;
 				when b=>
 					if(b_ok = '1')then
-						b_select_out <= b_select_in;
-						game_state <= judge;
+						if(a_select_in = 0)then
+							if(b_select_in = 1)then
+								a_score := a_score + 1;
+								if(a_score = 3)then
+									result <= 1;
+								end if;
+							elsif(b_select_in = 2)then
+								b_score := b_score +1;
+								if(b_score = 3)then
+									result <= 2;
+								end if;
+							end if;
+						elsif(a_select_in = 1)then
+							if(b_select_in = 2)then
+								a_score := a_score + 1;
+								if(a_score = 3)then
+									result <= 2;
+								end if;
+							elsif(b_select_in = 0)then
+								b_score := b_score +1;
+								if(b_score = 3)then
+									result <= 2;
+								end if;
+							end if;
+						elsif(a_select_in = 2)then
+							if(b_select_in = 0)then
+								a_score := a_score + 1;
+								if(a_score = 3)then
+									result <= 2;
+								end if;
+							elsif(b_select_in = 1)then
+								b_score := b_score +1;
+								if(b_score = 3)then
+									result <= 2;
+								end if;
+							end if;
+						end if;
+						a_score_out <= a_score;
+						b_score_out <= b_score;
+						game_state <= s0;
 					end if;
-				when judge=>
-					if(a_select_in = 0)then
-						if(b_select_in = 1)then
-							a_score <= a_score + 1;
-							if(a_score = 3)then
-								result <= 1;
-							end if;
-						elsif(b_select_in = 2)then
-							b_score <= b_score +1;
-							if(b_score = 3)then
-								result <= 2;
-							end if;
-						end if;
-					elsif(a_select_in = 1)then
-						if(b_select_in = 2)then
-							a_score <= a_score + 1;
-							if(a_score = 3)then
-								result <= 2;
-							end if;
-						elsif(b_select_in = 0)then
-							b_score <= b_score +1;
-							if(b_score = 3)then
-								result <= 2;
-							end if;
-						end if;
-					elsif(a_select_in = 2)then
-						if(b_select_in = 0)then
-							a_score <= a_score + 1;
-							if(a_score = 3)then
-								result <= 2;
-							end if;
-						elsif(b_select_in = 1)then
-							b_score <= b_score +1;
-							if(b_score = 3)then
-								result <= 2;
-							end if;
-						end if;
-					end if;
-					a_score_out <= a_score;
-					b_score_out <= b_score;
-					game_state <= s0;
 				when others =>
 					--do nothing;
 				end case;
@@ -160,9 +150,63 @@ architecture behave of SSP is
 		end process;
 end behave;
 		
-		
-		
-		
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity SSP is
+		port
+		(
+		clk_in:in std_logic;
+		start_in:in std_logic;
+		show:in std_logic;
+		a_score:in integer range 0 to 3;
+		b_score:in integer range 0 to 3;
+		digitron_cat:out std_logic_vector(5 downto 0);
+		digitron_show:out std_logic_vector(6 downto 0)
+		);
+end SSP;
+
+architecture behave of SSP is
+		signal counter:std_logic;
+		process(clk_in)
+		begin
+		variable a_score_tmp,b_score_tmp:integer range 0 to 3;
+		if(clk_in'event and clk_in = '1')then
+			if(start== '1')then
+				digitron_cat <= "000000";
+				digitron_show <= "0000001";
+			else
+				counter = not counter;
+				a_score_tmp := a_score;
+				b_score_tmp := b_score;
+				case count is
+					when '0'=>
+						digitron_cat <= "011111";
+							case a_score_tmp is
+								when 0=>digitron_show<="1111110";
+								when 1=>digitron_show<="0110000";
+								when 2=>digitron_show<="1101101";
+								when 3=>digitron_show<="1111001";
+								when others=>digitorn_show<="1111111";
+							end case;
+					when '1'=>
+						digitron_cat <= "101111";
+							case b_score_tmp is
+								when 0=>digitron_show<="1111110";
+								when 1=>digitron_show<="0110000";
+								when 2=>digitron_show<="1101101";
+								when 3=>digitron_show<="1111001";
+								when others=>digitorn_show<="1111111";
+							end case;
+					when others=>
+						digitron_cat<="0000001";
+					end case;
+				end if;
+			end if;
+		end if;
+	end process;
+end behave;
 		
 		
 		
